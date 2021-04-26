c2c8165b800e05333fddb30fa8859ffc3774ac4e
Michael CARRER <mic.carrer@gmail.com>
2021-04-26 16:29:05 +0200

    plop

--- utils/rcon.py
+++ utils/rcon.py
@@ -6,21 +6,26 @@ rcon_response_buffer_size = 65565
 
 
 def send(ip, port, password, rcon_request):
-    request = f'rcon {password} {rcon_request}\n'
-    request_bytes = request.encode()
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.connect((ip, port))
-    sock.send(rcon_request_prefix + request_bytes + rcon_request_suffix)
-    response_bytes = sock.recv(rcon_response_buffer_size)
-    sock.close()
+    try:
+      request = f'rcon {password} {rcon_request}\n'
+      request_bytes = request.encode()
 
-    response_bytes = response_bytes[len(rcon_request_prefix):]
-    response = response_bytes.decode()
+      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      sock.connect((ip, port))
+      sock.send(rcon_request_prefix + request_bytes + rcon_request_suffix)
+      response_bytes = sock.recv(rcon_response_buffer_size)
+      sock.close()
 
-    all_response_lines = response.splitlines()
+      response_bytes = response_bytes[len(rcon_request_prefix):]
+      response = response_bytes.decode()
 
-    response_type = all_response_lines[0]
-    response_lines = all_response_lines[1:]
+      all_response_lines = response.splitlines()
 
-    return (response_type, response_lines)
+      response_type = all_response_lines[0]
+      response_lines = all_response_lines[1:]
+
+      return (response_type, response_lines)
+    except print(0):
+        pass
+
