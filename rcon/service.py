00791e79a833b6bf519f45060e75de7013f9d769
Michael CARRER <mic.carrer@gmail.com>
2021-04-26 16:48:12 +0200

    plop

--- rcon/service.py
+++ rcon/service.py
@@ -1,12 +1,17 @@
+import socket
 from typing import Any, List
 from utils.colors import Colors
-from utils.rcon import send
 import time
+
+rcon_request_prefix = b'\xFF' * 4
+rcon_response_buffer_size = 65565
 
 class RconService:
   ip: str
   port: int
   password: str
+
+  sock: socket.socket
 
   color_info = Colors.LIGHT_BLUE.value
   color_success = Colors.GREEN.value
@@ -19,12 +24,38 @@ class RconService:
       self.ip = ip
       self.port = port
       self.password = password
+      
+      self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      self.sock.settimeout(2)
+      self.sock.connect((ip, port))
   
-  def send(self, request: str):
+  def __del__(self):
+    self.sock.close()
+
+  def send(self, rcon_request: str):
     while time.time() - self.last_send_time < self.delay:
       time.sleep(0.1)
     self.last_send_time = time.time()
-    return send(self.ip, self.port, self.password, request)
+
+    try:      
+      request = f'rcon {self.password} {rcon_request}\n'
+      request_bytes = request.encode()
+      
+      self.sock.send(rcon_request_prefix + request_bytes)
+      response_bytes = self.sock.recv(rcon_response_buffer_size)
+
+      response_bytes = response_bytes[len(rcon_request_prefix):]
+      response = response_bytes.decode()
+
+      all_response_lines = response.splitlines()
+
+      response_type = all_response_lines[0]
+      response_lines = all_response_lines[1:]
+
+      return (response_type, response_lines)
+    except print(0):
+        pass    
+
 
   def safe_msg(self, msg: str):
     return msg.replace('\"', '').split('\n')
